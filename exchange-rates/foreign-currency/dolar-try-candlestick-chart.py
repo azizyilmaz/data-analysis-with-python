356ff86c2ae32a7208599f963acb110e45783891
azizyilmaz <azizxyilmaz@outlook.com>
2020-07-09 01:48:27 +0300

    Daily Candlestick Chart of USD/TRY

---
+++ exchange-rates/foreign-currency/dolar-try-candlestick-chart.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from mpl_finance import candlestick_ohlc
+import pandas as pd
+import matplotlib.dates as mpl_dates
+
+plt.style.use('ggplot')
+
+data = pd.read_csv('exchange-rates/data/USD TRY Veri Geçmişi - Investing.com.csv')
+ohlc = data.loc[:, ['Tarih','Açılış','Yüksek','Düşük','Şimdi']]
+ohlc['Tarih'] = pd.to_datetime(ohlc['Tarih'])
+ohlc['Tarih'] = ohlc['Tarih'].apply(mpl_dates.date2num)
+ohlc = ohlc.astype(float)
+
+fig, ax = plt.subplots()
+
+candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
+
+ax.set_xlabel('Tarih')
+ax.set_ylabel('USD/TRY')
+fig.suptitle('Daily Candlestick Chart of USD/TRY between 2010 and 2020')
+
+date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+ax.xaxis.set_major_formatter(date_format)
+
+fig.autofmt_xdate()
+fig.tight_layout()
+
+plt.show()
