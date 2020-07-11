85b7d42da890a2cd5d4c3de284bfd6d7662c7676
azizyilmaz <azizxyilmaz@outlook.com>
2020-07-12 01:37:53 +0300

    Subplot code review

--- corona/plots/subplots.py
+++ corona/plots/subplots.py
@@ -3,21 +3,33 @@ import matplotlib.pyplot as plt
 
 dataset = pandas.read_csv('corona/coronavirusdata/covid_19_data.csv')
 
-turkey = dataset[dataset['Country/Region'] == 'Turkey']
-italy = dataset[dataset['Country/Region'] == 'Italy']
+turkey = dataset[dataset['Country/Region'] == 'Turkey'].groupby(
+    ['ObservationDate']).agg(
+        sum_Confirmed=('Confirmed', 'sum'),
+        sum_Deaths=('Deaths', 'sum'),
+    ).reset_index()
+italy = dataset[dataset['Country/Region'] == 'Italy'].groupby(
+    ['ObservationDate']).agg(
+        sum_Confirmed=('Confirmed', 'sum'),
+        sum_Deaths=('Deaths', 'sum'),
+    ).reset_index()
 
 dataset.plot(subplots=True)
 
 plt.subplot(2, 1, 1)
-plt.plot(turkey.Deaths, turkey.Recovered, color='red',
-         label='Deaths and Recovered in Turkey')
-plt.xlabel('Turkey Deaths')
-plt.ylabel('Turkey Recovered')
+plt.plot(turkey.sum_Confirmed,
+         turkey.sum_Deaths,
+         color='red',
+         label='Deaths and Confirmed in Turkey')
+plt.xlabel('Turkey Confirmed')
+plt.ylabel('Turkey Deaths')
 
 plt.subplot(2, 1, 2)
-plt.plot(italy.Deaths, italy.Recovered, color='blue',
-         label='Deaths and Recovered in Italy')
-plt.xlabel('Italy Deaths')
-plt.ylabel('Italy Recovered')
+plt.plot(italy.sum_Confirmed,
+         italy.sum_Deaths,
+         color='blue',
+         label='Deaths and Confirmed in Italy')
+plt.xlabel('Italy Confirmed')
+plt.ylabel('Italy Deaths')
 
 plt.show()
