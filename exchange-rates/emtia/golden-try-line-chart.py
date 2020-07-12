97a44855455788ce31f68e4a7e222295ae6bb9e5
azizyilmaz <azizxyilmaz@outlook.com>
2020-07-13 00:42:05 +0300

    XAUUSD

---
+++ exchange-rates/emtia/golden-try-line-chart.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Use 'Tarih' column as index
+ds = pd.read_csv(
+    'exchange-rates/data/Altın Vadeli İşlemleri Fiyat Geçmişi - Investing.com.csv')
+
+# Hide column
+ds = ds.drop('Açılış', axis=1)
+ds = ds.drop('Yüksek', axis=1)
+ds = ds.drop('Düşük', axis=1)
+ds = ds.drop('Hac.', axis=1)
+ds = ds.drop('Fark %', axis=1)
+
+# Change column's name
+ds.columns = ['Tarih', 'XAU/USD']
+
+# Reverse dataset's index
+ds = ds[::-1].reset_index(drop=True)
+print(ds)
+print(ds.dtypes)
+
+# Set 'Tarih' to index of dataset
+ds.index = pd.to_datetime(ds['Tarih'])
+print(ds)
+print(ds.dtypes)
+
+ds.plot(color='blue')
+
+plt.grid()
+plt.xlabel('Tarih')
+plt.ylabel('XAU')
+plt.title('Ons Altın kaç Dolar?')
+plt.legend()
+plt.show()
